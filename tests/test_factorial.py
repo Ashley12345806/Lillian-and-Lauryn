d0118e3338ea33ab4f179aaf5f0cacb3b3eef4db
Lillian Nakato <lillianashley2001@gmail.com>
2025-07-05 18:21:19 +0300

    added test factorial for 1

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -5,3 +5,8 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from factorial import factorial
 def test_factorial_for_0():
     assert factorial(0) == 1
+
+def test_factorial_for_1():
+    assert factorial(1) == 1
+
+
