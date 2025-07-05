c1928ca594656cf6c10a532326c009be6e660d3b
Lillian Nakato <lillianashley2001@gmail.com>
2025-07-05 16:08:49 +0300

    created first cycle

--- tests/test_multiply.py
+++ tests/test_multiply.py
@@ -2,3 +2,6 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from multiply import multiply
+
+def test_multiply_first_cycle():
+    assert multiply(1, 1) == 1
