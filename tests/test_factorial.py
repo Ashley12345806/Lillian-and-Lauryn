8ba028bfe018355584748d3be927bccc0582c556
Lillian Nakato <lillianashley2001@gmail.com>
2025-07-05 18:29:02 +0300

    added test factorial for 45

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -17,3 +17,6 @@ def test_factorial_for_3():
 
 def test_factorial_for_4():
     assert factorial(4) == 24
+
+def test_factorial_for_45():
+    assert factorial(45) == 11962222186597786209152000000000
